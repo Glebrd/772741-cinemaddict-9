258405148cfa2eb25c9b535049e7f8dc2553898c
Gleb Dolgushin <dolgushing@gmail.com>
2019-08-13 01:21:00 +0300

    правки

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import {getSearchMarkup} from './components/search.js';
 import {getUserRating} from './components/user-rating.js';
 import {getSorting} from './components/sorting.js';
 import {getMenu} from './components/menu.js';
+import {getFilms} from './components/films.js';
+import {getFilmDetails} from './components/film-details.js';
 
-const NUMBER_OF_CARDS = 3;
+const NUMBER_OF_MAIN_FILMS = 5;
+const NUMBER_OF_TOP_RATED_FILMS = 2;
+const NUMBER_OF_MOST_COMMENTED_FILMS = 2;
 
 const addBlock = (container, markup) => {
   container.insertAdjacentHTML(`beforeend`, markup);
@@ -11,7 +15,12 @@ const addBlock = (container, markup) => {
 
 const header = document.querySelector(`.header`);
 const main = document.querySelector(`.main`);
+const body = document.querySelector(`body`);
 
 // Форма поиска + Рейтинг пользователя
 addBlock(header, getSearchMarkup() + getUserRating());
-addBlock(main, getMenu() + getSorting());
+// Меню + Сортировка + Фильмы
+addBlock(main, getMenu() + getSorting() + getFilms(NUMBER_OF_MAIN_FILMS, NUMBER_OF_TOP_RATED_FILMS, NUMBER_OF_MOST_COMMENTED_FILMS));
+// Попап
+addBlock(body, getFilmDetails());
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,19 @@
 /* eslint-disable */
 
-const path = require(`path`);
+const path = require("path");
 module.exports = {
-  mode: `development`, // Режим сборки
-  entry: `./src/main.js`, // Точка входа приложения
-  output: {// Настройка выходного файла
-    filename: `bundle.js`,
-    path: path.join(__dirname, `public`)
+  mode: "development",
+  entry: "./src/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "public")
   },
-  devtool: `source-map`, // Подключаем
+  devtool: "source-map",
   devServer: {
-    contentBase: path.join(__dirname, `public`), // Где искать сборку
-    publicPath: 'http://localhost:8008/', // Веб адрес сборки
-    compress: true, // Сжатие
-    // Автоматическая перезагрузка страницы
-    // Если не работает по стандартному URLу в браузере ‘http://localhost:8080’,
-    // то добавьте к нему ‘/webpack-dev-server/‘: ‘http:!/localhost:8080/webpack-dev-server/'
+    contentBase: path.join(__dirname, "public"),
+    publicPath: "http://localhost:8090/",
+    port: 8090,
+    compress: true,
     watchContentBase: true
-    }
+  }
 };
-
