c99b1d9f15a23fee651afc4ed353934ee1192939
Gleb Dolgushin <dolgushing@gmail.com>
2019-09-07 21:03:28 +0300

    правки

---
+++ src/components/films-most-commented.js
@@ -0,0 +1,10 @@
+import {AbstractComponent} from './abstract-component';
+export class FilmsMostCommented extends AbstractComponent {
+  getTemplate() {
+    return `<section class="films-list--extra">
+          <h2 class="films-list__title">Most commented</h2>
+          <div class="films-list__container films-list__container--commented">
+          </div>
+        </section>`;
+  }
+}
